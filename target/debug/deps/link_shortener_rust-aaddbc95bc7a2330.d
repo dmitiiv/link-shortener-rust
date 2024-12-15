ba6cc6f6d9c28404e368d9341d7c3c25a48350a6
davarlamov <davarlamov@avito.ru>
2024-12-15 13:12:29 +0300

    feat: start project

---
+++ target/debug/deps/link_shortener_rust-aaddbc95bc7a2330.d
@@ -0,0 +1,5 @@
+/Users/davarlamov/User/homework/link-shortener-rust/target/debug/deps/liblink_shortener_rust-aaddbc95bc7a2330.rmeta: src/main.rs
+
+/Users/davarlamov/User/homework/link-shortener-rust/target/debug/deps/link_shortener_rust-aaddbc95bc7a2330.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/link_shortener_rust-fd11832e709b7af5.d
@@ -0,0 +1,5 @@
+/Users/davarlamov/User/homework/link-shortener-rust/target/debug/deps/liblink_shortener_rust-fd11832e709b7af5.rmeta: src/main.rs
+
+/Users/davarlamov/User/homework/link-shortener-rust/target/debug/deps/link_shortener_rust-fd11832e709b7af5.d: src/main.rs
+
+src/main.rs:
